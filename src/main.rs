0968ef8e834619eec339fb8f545f25215ba5a18e
Omega1902 <27062486+Omega1902@users.noreply.github.com>
2024-03-14 22:29:53 +0100

    format numbers with german locale

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io;
 
 use ::num::integer::Roots;
 use bitvec::prelude::*;
+use num_format::{Locale, ToFormattedString};
 
 trait PrimCalc {
     fn is_prim(num: usize) -> bool;
@@ -52,26 +53,28 @@ impl PrimCalc for Sieve {
 }
 
 fn print_is_prim(number: usize) {
-    println!("{number} is a prim: {}", Sieve::is_prim(number));
+    println!("{} is a prim: {}", number.to_formatted_string(&Locale::de), Sieve::is_prim(number));
 }
 
 fn main() {
     loop {
-        println!("Type in the next variable to check if it is a prim");
+        println!("Type in the next positive integer to check if it is a prim");
         let mut input = String::new();
         let input_length = io::stdin().read_line(&mut input);
         if input_length.is_err() {
             println!("Error reading line");
             continue;
         }
-        match input.trim().parse::<usize>() {
+        match input.trim().replace("_", "").parse::<usize>() {
             Ok(input_number) => print_is_prim(input_number),
             Err(_) => println!("Not possible to convert '{}' into a positive integer", input.trim()),
         }
     }
-    // for i in [2, 20, 131, 1_000_001, 10_000_001, 100_000_001, 1_000_000_007, 10_000_000_001] {
-    //     print_is_prim(i);
-    // }
+    // 1_000_001
+    // 10_000_001
+    // 100_000_001
+    // 1_000_000_007
+    // 10_000_000_001
 }
 
 #[cfg(test)]
@@ -88,9 +91,19 @@ mod tests {
 
     #[test]
     fn test_sieve() {
+        assert_eq!(Sieve::is_prim(0), false);
+        assert_eq!(Sieve::is_prim(1), false);
         assert_eq!(Sieve::is_prim(2), true);
         assert_eq!(Sieve::is_prim(3), true);
         assert_eq!(Sieve::is_prim(4), false);
+        assert_eq!(Sieve::is_prim(5), true);
+        assert_eq!(Sieve::is_prim(6), false);
+        assert_eq!(Sieve::is_prim(7), true);
+        assert_eq!(Sieve::is_prim(8), false);
+        assert_eq!(Sieve::is_prim(9), false);
+        assert_eq!(Sieve::is_prim(10), false);
+        assert_eq!(Sieve::is_prim(11), true);
+        assert_eq!(Sieve::is_prim(12), false);
         assert_eq!(Sieve::is_prim(25), false);
         assert_eq!(Sieve::is_prim(131), true);
         assert_eq!(Sieve::is_prim(1_000_001), false);
